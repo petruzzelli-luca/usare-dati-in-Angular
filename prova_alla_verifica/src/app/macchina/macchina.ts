dd1c929fab35d5a1893961f7587709a001bce079
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-10-17 20:09:50 +0000

    punto 3 della simulazione alla verifica

--- prova_alla_verifica/src/app/macchina/macchina.ts
+++ prova_alla_verifica/src/app/macchina/macchina.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-macchina',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './macchina.css'
 })
 export class Macchina {
-
+  
 }
